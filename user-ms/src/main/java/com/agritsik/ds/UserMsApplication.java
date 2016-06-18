2887c7e63b952d5f405b186d674d78f4474da89b
andrey <a.gritsik@gmail.com>
2016-06-18 23:17:53 +0300

    add more files

---
+++ user-ms/src/main/java/com/agritsik/ds/UserMsApplication.java
@@ -0,0 +1,12 @@
+package com.agritsik.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsApplication.class, args);
+	}
+}
