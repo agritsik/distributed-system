93def16e6da55b1992b97f34b1fa1cc2a0ec0d32
andrey <a.gritsik@gmail.com>
2016-06-19 11:41:21 +0300

    add legacy system, remove test app

---
+++ legacy-client-ms/src/main/java/com/agritsik/ds/LegacyClientMsApplication.java
@@ -0,0 +1,12 @@
+package com.agritsik.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyClientMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyClientMsApplication.class, args);
+	}
+}
---
+++ legacy-rest-ms/src/main/java/com/agritsik/ds/LegacyRestMsApplication.java
@@ -0,0 +1,12 @@
+package com.agritsik.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyRestMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyRestMsApplication.class, args);
+	}
+}
