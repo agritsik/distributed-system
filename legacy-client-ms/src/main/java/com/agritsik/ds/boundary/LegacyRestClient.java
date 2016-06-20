826548e53b112134979b2fd9b8d2f1904fbe3105
andrey <a.gritsik@gmail.com>
2016-06-20 14:32:45 +0300

    hystrix support

--- legacy-client-ms/src/main/java/com/agritsik/ds/boundary/LegacyRestClient.java
+++ legacy-client-ms/src/main/java/com/agritsik/ds/boundary/LegacyRestClient.java
@@ -1,5 +1,6 @@
 package com.agritsik.ds.boundary;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,11 +10,16 @@ import java.net.URI;
 public class LegacyRestClient {
 
 
+    @HystrixCommand(fallbackMethod = "reliable")
     public String ping(){
         RestTemplate restTemplate = new RestTemplate();
         return ">>> " + restTemplate.getForObject(
                 URI.create("http://localhost:9001/"),
                 String.class);
 
+    }
+
+    public String reliable(){
+        return "circuit breaker";
     }
 }
--- legacy-client-ms/src/test/java/com/agritsik/ds/LegacyClientMsApplicationTests.java
+++ legacy-client-ms/src/test/java/com/agritsik/ds/LegacyClientMsApplicationTests.java
@@ -2,15 +2,17 @@ package com.agritsik.ds;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LegacyClientMsApplication.class)
+@WebAppConfiguration
 public class LegacyClientMsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
